791e59319d2fb041868f2dc5e410f19e20b1b82f
jimbo831 <jimbo831@gmail.com>
2018-07-20 13:16:16 -0500

    Initial commit, new project from auto Spring initializer

---
+++ src/main/java/com/johnbwhitejr/target/casestudy/MyRetailCaseStudyApplication.java
@@ -0,0 +1,12 @@
+package com.johnbwhitejr.target.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRetailCaseStudyApplication.class, args);
+    }
+}
